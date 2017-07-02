c5d68b8014299e07eccbc476daf5154050ecd694
mynameisvinn <vin.tang@gmail.com>
2017-07-02 09:00:51 -0400

    test calc_hash_seal

--- PyBlockchain/__init__.py
+++ PyBlockchain/__init__.py
@@ -2,3 +2,4 @@ from miner import Miner
 from node import Node
 from block import Block
 from blockchain import Blockchain
+from baseblock import BaseBlock
--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,31 @@
 import unittest
-from PyBlockchain import Blockchain
+from PyBlockchain import Blockchain, Block, BaseBlock, Node
 
 class Test_Blockchain(unittest.TestCase):
     """test module for learner
     """
     def setUp(self):
+        """set up a blockchain with a single genesis block.
         """
-        create an instance of learner to be tested
-        """
-        genesis_block = "A"
-        self.blockchain = Blockchain(genesis_block)
+        index = 0
+        prev_hash = "hello"
+        data = " World"
+        curr_hash = "placeholder"
+
+        self.genesis_block = Block(index, prev_hash, data, curr_hash)
+        self.blockchain = Blockchain(self.genesis_block)
+
 
     def test_get_length(self):
-        """test
-        """
         self.assertEqual(self.blockchain.get_length(), 1)
+
+    def test_calc_hash_seal(self):
+        """sha1("hello World11") will satisfy proof of work.
+        """
+        self.assertEqual(BaseBlock.calc_hash_seal(self.genesis_block), "11")
+
+    def test_check_hash_validity(self):
+
 
 if __name__ == "__main__":
     unittest.main()
