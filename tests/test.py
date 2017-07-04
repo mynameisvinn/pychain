fdd4087605d42ce7dda017f174bda248f4646f1b
mynameisvinn <vin.tang@gmail.com>
2017-07-03 21:22:49 -0400

    include test for miner update

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 """tests"""
 import unittest
-from PyBlockchain import Blockchain, Block, BaseBlock, Node
+from PyBlockchain import Blockchain, Block, BaseBlock, Node, Miner
 
 class Test_Blockchain(unittest.TestCase):
     """test module for learner
@@ -18,6 +18,7 @@ class Test_Blockchain(unittest.TestCase):
         self.blockchain = Blockchain(self.genesis_block)
         self.my_node = Node(self.blockchain)
         self.new_block = self.my_node.generate_new_block(new_data)
+        self.my_miner = Miner()
 
     def test_get_length(self):
         self.assertEqual(self.blockchain.get_length(), 1)
@@ -31,8 +32,14 @@ class Test_Blockchain(unittest.TestCase):
         """new blocks are always rejected until a miner can calculate the
         correct sealing hash.
         """
-        self.assertEqual(self.my_node.check_block(self.new_block), False)
+        self.assertEqual(self.my_node.check_block(self.genesis_block), False)
 
+    def test_miner_update(self):
+        # update genesis block with the correct hash
+        self.my_miner.update_hash(self.genesis_block, "11")
+
+        # node will reconstruct hash from block's prev_hash and data
+        self.assertEqual(self.my_node._check_hash_validity(self.genesis_block), True)
 
 if __name__ == "__main__":
     unittest.main()
