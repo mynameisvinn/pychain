f36951fa8c0e3dc08db95846de443a47b7c0fa2a
Vincent Tang <Vincent@Admins-MacBook-Pro-8.local>
2017-08-21 09:19:31 -0400

    incorporate merkle trees

--- PyBlockchain/block.py
+++ PyBlockchain/block.py
@@ -1,5 +1,8 @@
 from hashlib import sha256
 
+def h(msg):
+    return sha256(msg).hexdigest()  # in bitcoin's merkle trees, SHA256 is applied twice (known as double-SHA256)
+
 class Block(object):
     """
     a blockchain is composed of blocks. each block is represented by a 
@@ -19,7 +22,16 @@ class Block(object):
 
     @property
     def root_hash(self):
-        a, b, c, d = map(lambda x: sha256(x).hexdigest(), self.ls_transactions)
-        e = sha256(a + b).hexdigest()
-        f = sha256(c + d).hexdigest()
-        return sha256(e + f).hexdigest()
+        q = self.ls_transactions
+
+        # if number of transactions is odd, duplicate last transaction
+        n_transactions = len(q)
+        if n_transactions % 2 == 1:
+            q.append(q[n_transactions - 1])
+
+        while len(q) > 1:
+            a, b = q.pop(0), q.pop(0)
+            c = h(a + b)
+            q.append(c)
+            print q
+        return q[0]
