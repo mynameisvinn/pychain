dd7ce14f7c1902aa8b66acd12f20abeae58baabb
mynameisvinn <vin.tang@gmail.com>
2018-02-26 08:22:23 -0500

    update notebook with better comments

--- PyBlockchain/blockchain.py
+++ PyBlockchain/blockchain.py
@@ -5,6 +5,10 @@ class Blockchain(object):
     """blockchain object.
     """
     def __init__(self, genesis_block):
+        """
+        in this example, blockchain will be a python list consisting 
+        of Block objects. 
+        """
         self.blockchain = [genesis_block]
 
     def get_length(self):
